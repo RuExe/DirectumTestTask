6d0ca86cf00e6d85981e76ec6aa38d1d90a46aec
RuExe <ru.exe.grizzly@mail.ru>
2021-02-15 22:41:57 +0700

    TextReporter Refactoring

--- DirectumTestTask/TextReporter.cs
+++ DirectumTestTask/TextReporter.cs
@@ -9,16 +9,16 @@ namespace DirectumTestTask
     {
         public static void Report(List<ExecutorItem> executorItems, string filePath)
         {
+            int rkkCount = executorItems.Sum(item => item.RkkCount);
+            int appealCount = executorItems.Sum(item => item.AppealCount);
+
+            int countLength = executorItems.Count.ToString().Length;
+
             using (StreamWriter sw = new StreamWriter(filePath))
             {
-                int rkkCount = executorItems.Sum(item => item.RkkCount);
-                int appealCount = executorItems.Sum(item => item.AppealCount);
-
                 sw.WriteLine("Не исполнено в срок " + (rkkCount + appealCount) + " документов, из них:" + Environment.NewLine +
                     "- количество неисполненных входящих документов: " + rkkCount + ";" + Environment.NewLine +
                     "- количество неисполненных письменных обращений граждан: " + appealCount + "." + Environment.NewLine);
-
-                int countLength = executorItems.Count.ToString().Length;
 
                 string firstTitle = "Ответственный исполнитель";
                 string secondTitle = "Количество неисполненных входящих документов";
@@ -28,19 +28,19 @@ namespace DirectumTestTask
                 sw.Write(firstTitle + "\t");
                 sw.Write(secondTitle + "\t");
                 sw.Write(thirdTitle + "\t");
-                sw.Write("Общее количество документов и обращений" + Environment.NewLine);
+                sw.WriteLine("Общее количество документов и обращений");
 
                 int index = 1;
                 foreach (ExecutorItem executor in executorItems)
                 {
-                    sw.Write(index++.ToString() + new string(' ', countLength - (index.ToString().Length - 1)) + "\t");
-                    sw.Write(executor.Executor.InitialsFullName + new string(' ', firstTitle.Length - executor.Executor.InitialsFullName.Length) + "\t");
-                    sw.Write(executor.RkkCount.ToString() + new string(' ', secondTitle.Length - executor.RkkCount.ToString().Length) + "\t");
-                    sw.Write(executor.AppealCount.ToString() + new string(' ', thirdTitle.Length - executor.AppealCount.ToString().Length) + '\t');
-                    sw.Write((executor.Sum).ToString() + '\t' + Environment.NewLine);
+                    sw.Write(index++.ToString() + "\t".PadLeft(countLength - (index.ToString().Length - 1)));
+                    sw.Write(executor.Executor.InitialsFullName + "\t".PadLeft(firstTitle.Length - executor.Executor.InitialsFullName.Length));
+                    sw.Write(executor.RkkCount.ToString() + "\t".PadLeft(secondTitle.Length - executor.RkkCount.ToString().Length));
+                    sw.Write(executor.AppealCount.ToString() + "\t".PadLeft(thirdTitle.Length - executor.AppealCount.ToString().Length));
+                    sw.WriteLine(executor.Sum);
                 }
 
-                sw.WriteLine(Environment.NewLine + "Дата составления справки: " + DateTime.Now.ToString("dd.MM.yyyy"));
+                sw.Write(Environment.NewLine + "Дата составления справки: " + DateTime.Now.ToString("dd.MM.yyyy"));
             }
         }
     }
